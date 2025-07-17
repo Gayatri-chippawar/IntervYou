a14a3abe5e900f1b270e8738851ee0ac5c39339a
Gayatri Chippawar <gayatrichippawar@gmail.com>
2025-07-17 10:04:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://intervyou-frontend.onrender.com");
+  res.header("Access-Control-Allow-Origin", "https://interv-you-frontend-gayatri-chippawars-projects.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
